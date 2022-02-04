996cb38a3d79c4c16b4b161eabd9a8cdfb4d723a
VidunderGunder <kristiankramas@outlook.com>
2022-02-04 13:49:44 +0100

    Head Estimation

--- src/game/PoseMesh.tsx
+++ src/game/PoseMesh.tsx
@@ -6,22 +6,26 @@ import { GameStore } from "../Store";
 const sizes: {
   [key: string]: number;
 } = {
+  head: 0.25, // Deduced joint
+
   nose: 0.025,
 
-  left_eye: 0.01,
-  right_eye: 0.01,
-  left_eye_inner: 0.01,
-  right_eye_inner: 0.01,
-  left_eye_outer: 0.01,
-  right_eye_outer: 0.01,
-  left_ear: 0.025,
-  right_ear: 0.025,
+  left_eye_inner: 0.025,
+  left_eye: 0.0375,
+  right_eye_outer: 0.025,
+
+  right_eye_inner: 0.025,
+  right_eye: 0.0375,
+  left_eye_outer: 0.025,
+
+  left_ear: 0.05,
+  right_ear: 0.05,
 
   mouth_left: 0,
   mouth_right: 0,
 
-  left_shoulder: 0.15,
-  right_shoulder: 0.15,
+  left_shoulder: 0.125,
+  right_shoulder: 0.125,
   left_elbow: 0.1,
   right_elbow: 0.1,
   left_wrist: 0.1,
@@ -33,6 +37,24 @@ const sizes: {
   left_ankle: 0.1,
   right_ankle: 0.1,
 };
+
+const hiddenJoints = [
+  "nose",
+
+  "left_eye_inner",
+  "left_eye",
+  "left_eye_outer",
+
+  "right_eye_inner",
+  "right_eye",
+  "right_eye_outer",
+
+  "mouth_left",
+  "mouth_right",
+
+  "left_ear",
+  "right_ear",
+];
 
 const jointsToConnect = [
   ["left_shoulder", "left_elbow"],
@@ -47,14 +69,6 @@ const jointsToConnect = [
   ["left_shoulder", "left_hip"],
   ["right_shoulder", "right_hip"],
   ["left_hip", "right_hip"],
-
-  ["left_eye", "left_eye_inner"],
-  ["left_eye_outer", "left_eye_inner"],
-
-  ["right_eye", "right_eye_inner"],
-  ["right_eye_outer", "right_eye_inner"],
-
-  ["mouth_left", "mouth_right "],
 ];
 
 export function PoseMesh({
@@ -104,9 +118,56 @@ export function PoseMesh({
     </>
   );
 
+  /**
+   * Sphere between ears
+   */
+  const Head = () => {
+    const pointA = pose.keypoints3D?.find((p) => p.name === "left_ear");
+    const pointB = pose.keypoints3D?.find((p) => p.name === "right_ear");
+
+    if (pointA === undefined || pointB === undefined) return null;
+
+    const vectorA = new Vector3(
+      pointA?.x ?? 0,
+      -(pointA?.y ?? 0) + 0.825,
+      pointA?.z ?? 0
+    );
+    const vectorB = new Vector3(
+      pointB?.x ?? 0,
+      -(pointB?.y ?? 0) + 0.825,
+      pointB?.z ?? 0
+    );
+
+    const score = ((pointA.score ?? 0) + (pointB.score ?? 0)) / 2;
+
+    const key = [name ?? "unnamed-pose", "head"].join("-");
+
+    return (
+      <sphere
+        key={key}
+        name={key}
+        diameter={sizes.head}
+        position={vectorA.add(vectorB).scale(0.5)}
+        {...sphereProps}
+      >
+        <standardMaterial
+          name={[key, "material"].join("-")}
+          diffuseColor={Color3.FromArray([
+            Math.max((2 - score * 2) * 0.75, 0),
+            Math.max(score, 0),
+            0.42,
+          ])}
+          alpha={score}
+        />
+      </sphere>
+    );
+  };
+
   return (
     <mesh name={name}>
       {pose.keypoints3D?.map((point, i) => {
+        if (typeof point.name === "string" && hiddenJoints.includes(point.name))
+          return null;
         if (!point.score || point.score < threshold) return null;
 
         const size = sizes[point.name ?? ""];
@@ -121,7 +182,7 @@ export function PoseMesh({
             <sphere
               key={key}
               name={key}
-              diameter={size ?? 0.0675}
+              diameter={size ?? 0.05}
               setEnabled={size !== 0}
               segments={8}
               position={new Vector3(point.x, -point.y + 0.825, point.z)}
@@ -140,6 +201,7 @@ export function PoseMesh({
           </>
         );
       })}
+      <Head />
       <Lines />
     </mesh>
   );
